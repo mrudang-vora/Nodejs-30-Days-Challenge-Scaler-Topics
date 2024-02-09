3fdb4a35a35ae8ace62b0ef68f5bb8a2f18433cc
Mrudang Vora <mrudang-vora@users.noreply.github.com>
2024-02-10 00:16:23 +0530

    Added Solution for Day 9 Node JS Challenge

--- solutions/answer9.cjs
+++ solutions/answer9.cjs
@@ -13,5 +13,8 @@ d. All of the above
 Answer:
 All of the above
 
-All of them are node modules
+All of them are node modules (Chat GPT explanations below)
+Express - web application framework for Node.js that provides a robust set of features for web and mobile applications.
+Body-parser - middleware for Express that extracts the body portion of an incoming request and exposes it on req.body as an object
+Socket.io - library that enables real-time, bidirectional, and event-based communication between web clients and servers using WebSockets or other compatible transport mechanisms.
 */
