99794090abb585b4e2cd2dc4da5461c22f76eb81
Mrudang Vora <mrudang-vora@users.noreply.github.com>
2024-02-10 00:16:00 +0530

    Added Solution for Day 9 Node JS Challenge

---
+++ solutions/answer9.cjs
@@ -0,0 +1,17 @@
+/*
+Question:
+Which of the following are examples of node modules?
+
+a. Express
+
+b. Body-parser
+
+c. Socket.io
+
+d. All of the above
+
+Answer:
+All of the above
+
+All of them are node modules
+*/
